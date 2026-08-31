456c8933dfe113cb3d3a29fb1c0d0a514d141aed
Rafael Molina <r.molina@parks.swiss>
2026-08-31 15:53:51 +0200

    feat: add outdooractive route extract for tests and route layer toggle

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const Map = await $arcgis.import("@arcgis/core/Map.js");
 const Basemap = await $arcgis.import("@arcgis/core/Basemap.js");
+const FeatureLayer = await $arcgis.import("@arcgis/core/layers/FeatureLayer.js");
 const WMTSLayer = await $arcgis.import("@arcgis/core/layers/WMTSLayer.js");
+
+const routeRenderer = {
+	type: "simple",
+	symbol: {
+		type: "simple-line",
+		color: [255, 110, 50, 1],
+		width: 4,
+		style: "solid",
+	},
+};
 
 const viewElement = document.querySelector("arcgis-map");
 viewElement.spatialReference = { wkid: 2056 };
 viewElement.center = {
 	type: "point",
-	x: 2657560,
-	y: 1194592,
+	x: 2655870,
+	y: 1135174,
 	spatialReference: {
 		wkid: 2056,
 	},
 };
-viewElement.scale = 1500000;
+viewElement.scale = 50000;
+
+const outdooractiveRoutes = new FeatureLayer({
+	url: "https://services1.arcgis.com/46913CWHRFmfQUln/arcgis/rest/services/oa_routenauszug_fuer_tests/FeatureServer/0",
+	outFields: ["*"],
+	opacity: 0.9,
+	title: "Outdooractive test routes",
+	renderer: routeRenderer,
+	visible: true,
+});
 
 const swisstopoWmts = new WMTSLayer();
 swisstopoWmts.url = "https://wmts.geo.admin.ch/EPSG/2056/1.0.0/WMTSCapabilities.xml";
@@ -38,12 +58,18 @@ const basemapSwissimage = new Basemap({
 
 viewElement.map = new Map({
 	basemap: basemapPixelkarte,
+	layers: [outdooractiveRoutes],
 });
 await viewElement.viewOnReady();
 
 const basemapGallery = document.querySelector("arcgis-basemap-gallery");
 basemapGallery.source = [basemapPixelkarte, basemapPixelkarteGrau, basemapSwissimage];
 basemapGallery.activeBasemap = basemapPixelkarte;
+
+const routeLayerToggle = document.getElementById("route-layer-toggle");
+routeLayerToggle.addEventListener("change", () => {
+	outdooractiveRoutes.visible = routeLayerToggle.checked;
+});
 
 function createSwisstopoWmts(layerId) {
 	const wmts = new WMTSLayer();
