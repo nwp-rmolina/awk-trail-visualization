cc38e885ca5e755ec8c0c4f31ba2c60b9dbbda34
Rafael Molina <r.molina@parks.swiss>
2026-08-31 16:21:45 +0200

    feat: add scale-dependent rendering adjustments on trail symbology

--- script.js
+++ script.js
@@ -5,47 +5,59 @@ const WMTSLayer = await $arcgis.import("@arcgis/core/layers/WMTSLayer.js");
 const UniqueValueRenderer = await $arcgis.import("@arcgis/core/renderers/UniqueValueRenderer.js");
 const CIMSymbol = await $arcgis.import("@arcgis/core/symbols/CIMSymbol.js");
 
-const walkSymbol = new CIMSymbol({
-	data: {
-		type: "CIMSymbolReference",
-		symbol: {
-			type: "CIMLineSymbol",
-			symbolLayers: [
-				{
-					type: "CIMSolidStroke",
-					enable: true,
-					capStyle: "ROUND",
-					joinStyle: "ROUND",
-					width: 1,
-					color: [255, 255, 255, 255],
-				},
-				{
-					type: "CIMSolidStroke",
-					enable: true,
-					capStyle: "ROUND",
-					joinStyle: "ROUND",
-					width: 2.5,
-					color: [190, 226, 98, 255],
-				},
-				{
-					type: "CIMSolidStroke",
-					enable: true,
-					capStyle: "ROUND",
-					joinStyle: "ROUND",
-					width: 4,
-					color: [153, 188, 66, 255],
-				},
-			],
+function createWalkSymbol(scale) {
+	let scaleFactor = 1;
+
+	if (scale <= 20000) {
+		scaleFactor = 1.75;
+	} else if (scale <= 50000) {
+		scaleFactor = 1.5;
+	} else if (scale <= 100000) {
+		scaleFactor = 1.25;
+	}
+
+	return new CIMSymbol({
+		data: {
+			type: "CIMSymbolReference",
+			symbol: {
+				type: "CIMLineSymbol",
+				symbolLayers: [
+					{
+						type: "CIMSolidStroke",
+						enable: true,
+						capStyle: "ROUND",
+						joinStyle: "ROUND",
+						width: 1 * scaleFactor,
+						color: [255, 255, 255, 255],
+					},
+					{
+						type: "CIMSolidStroke",
+						enable: true,
+						capStyle: "ROUND",
+						joinStyle: "ROUND",
+						width: 2.5 * scaleFactor,
+						color: [190, 226, 98, 255],
+					},
+					{
+						type: "CIMSolidStroke",
+						enable: true,
+						capStyle: "ROUND",
+						joinStyle: "ROUND",
+						width: 4 * scaleFactor,
+						color: [153, 188, 66, 255],
+					},
+				],
+			},
 		},
-	},
-});
+	});
+}
 
 const routeRenderer = new UniqueValueRenderer({
 	field: "kategorie",
 	uniqueValueInfos: [
 		{
 			value: "wanderung",
-			symbol: walkSymbol,
+			symbol: createWalkSymbol(50000),
 		},
 	],
 });
@@ -107,6 +119,20 @@ const routeLayerToggle = document.getElementById("route-layer-toggle");
 routeLayerToggle.addEventListener("change", () => {
 	outdooractiveRoutes.visible = routeLayerToggle.checked;
 });
+
+const updateRouteSymbolForScale = () => {
+	const currentScale = viewElement.view.scale;
+	routeRenderer.uniqueValueInfos = [
+		{
+			value: "wanderung",
+			symbol: createWalkSymbol(currentScale),
+		},
+	];
+	outdooractiveRoutes.renderer = routeRenderer;
+};
+
+viewElement.view.watch("scale", updateRouteSymbolForScale);
+updateRouteSymbolForScale();
 
 function createSwisstopoWmts(layerId) {
 	const wmts = new WMTSLayer();
